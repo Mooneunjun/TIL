bfa591ce4e7908516dd00c4906774db5a063b769
Mooneunjun <mooneunjun@icloud.com>
2024-11-29 01:20:22 +0900

    Add: 숫자 자료형에 대한 설명 및 예제 추가

--- data-types/00-number/number.js
+++ data-types/00-number/number.js
@@ -16,3 +16,27 @@ let product = positiveInt * 2;
 let quotient = pi / 2;
 
 console.log(sum, difference, product, quotient); // 13.14 -11.14 20 1.57
+
+// 지수 표기법 (Exponential Notation)
+const largeNumber = 1e9; // 10억
+const smallNumber = 1.23e-3; // 0.00123
+
+console.log(largeNumber); // 출력: 1000000000
+console.log(smallNumber); // 출력: 0.00123
+
+// 지수 표기법 비교
+const numA = 1e2; // 100
+const numB = 100;
+console.log(numA === numB); // 출력: true
+
+// 16진수 (Hexadecimal)
+const hex = 0x1f; // 16진수 1F
+console.log(hex); // 출력: 31
+
+// 8진수 (Octal)
+const oct = 0o37; // 8진수 37
+console.log(oct); // 출력: 31
+
+// 2진수 (Binary)
+const bin = 0b11111; // 2진수 11111
+console.log(bin); // 출력: 31
